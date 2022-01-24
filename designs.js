ca8090fdb0af71f803b21239307c130d180b93cc
rtaneja24 <98236764+rtaneja24@users.noreply.github.com>
2022-01-23 23:26:43 -0800

    added new code
    added new code

--- designs.js
+++ designs.js
@@ -4,14 +4,25 @@ const color = document.getElementById("colorPicker");
 const height = document.getElementById("inputHeight");
 const width = document.getElementById("inputWidth");
 const canvas = document.getElementById("pixelCanvas");
+const submitButton = document.getElementById('submitButton')
+const sizePicker = document.getElementById("sizePicker");
 
 // When size is submitted by the user, call makeGrid()
+sizePicker.addEventListener('submit', function(event)
+{
+  event.preventDefault();
+  makeGrid(height, width);
+  }
+)
 
-function makeGrid() {
-  for(i = 0; i <= height; i++) {
+)
+function makeGrid(height, width) {
+  canvas.createElement("table")
+  for(let i = 0; i <= height; i++) {
     let row = canvas.insertRow(i);
   }
-  for(j=0; j < =width; j++) {
+  for(let j=0; j <= width; j++) {
     let column = canvas.insertCell(j);
   }
 }
+// still need to make the table interactive once we have a functional table
