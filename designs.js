940102bfe23adf0c7089cb5b087244c24cab8c52
rtaneja24 <98236764+rtaneja24@users.noreply.github.com>
2022-01-23 22:48:39 -0800

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,17 @@
 // Select color input
 // Select size input
+const color = document.getElementById("colorPicker");
+const height = document.getElementById("inputHeight");
+const width = document.getElementById("inputWidth");
+const canvas = document.getElementById("pixelCanvas");
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
-// Your code goes here!
-
+  for(i = 0; i <= height; i++) {
+    let row = canvas.insertRow(i);
+  }
+  for(j=0; j < =width; j++) {
+    let column = canvas.insertCell(j);
+  }
 }
