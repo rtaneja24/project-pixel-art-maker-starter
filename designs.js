e09de8e52e0d6e48df7554b12dfe90ac59d76960
rtaneja24 <98236764+rtaneja24@users.noreply.github.com>
2022-01-24 09:20:47 -0800

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,28 +1,34 @@
 // Select color input
 // Select size input
-const color = document.getElementById("colorPicker");
-const height = document.getElementById("inputHeight");
-const width = document.getElementById("inputWidth");
 const canvas = document.getElementById("pixelCanvas");
 const submitButton = document.getElementById('submitButton')
-const sizePicker = document.getElementById("sizePicker");
 
 // When size is submitted by the user, call makeGrid()
-sizePicker.addEventListener('submit', function(event)
+document.addEventListener('submit', function(event)
 {
   event.preventDefault();
+  canvas.innerHTML = "";
+  const height = document.getElementById("inputHeight").value;
+  const width = document.getElementById("inputWidth").value;
   makeGrid(height, width);
+  console.log('wemadadafdfasdfada')
+  console.log(height)
+  console.log(width)
+
   }
 )
 
-)
 function makeGrid(height, width) {
-  canvas.createElement("table")
-  for(let i = 0; i <= height; i++) {
-    let row = canvas.insertRow(i);
-  }
-  for(let j=0; j <= width; j++) {
-    let column = canvas.insertCell(j);
+  const table = document.createElement("TABLE");
+  const color = document.getElementById("colorPicker").value;
+  for(let i = 0; i < height; i++) {
+    let row = document.createElement('tr');
+    canvas.appendChild(row);
+    for(let j=0; j < width; j++) {
+      let cell = document.createElement('td');
+    row.appendChild(cell);
+    cell.addEventListener('click', function (event){event.target.style.backgroundColor = color})
+    }
   }
 }
 // still need to make the table interactive once we have a functional table
